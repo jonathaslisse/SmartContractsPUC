2f9a494363a3cf9dd065308c9a0cae0e26a74a43
Jonathas Lisse <92129685+jonathaslisse@users.noreply.github.com>
2021-11-08 23:18:11 -0300

    Update CompraVendaVeiculo.sol

--- CompraVendaVeiculo.sol
+++ CompraVendaVeiculo.sol
@@ -28,13 +28,15 @@ contract CompraVendaVeiculos {
     uint public saldoAberto;
     uint public dataVencimento;
     
+    event PagamentoEntrada(address _comprador, uint _valorPagamento);
+    event PagamentoParcela(address _comprador, uint _valorPagamento);
+    
     
     constructor(
         
         uint _valorTotal,
         uint _valorEntrada,
         uint _qtdParcelas,
-        uint _juros,
         uint _correcaoMonetaria,
         
         string memory _placaVeiculo,
@@ -53,7 +55,7 @@ contract CompraVendaVeiculos {
         placaVeiculo = _placaVeiculo;
         renavanVeiculo = _renavanVeiculo;
         estadoVeiculo = _estadoVeiculo;
-        juros = _juros;
+        juros = 1;
         correcaoMonetaria = _correcaoMonetaria;
         saldoAberto = valorTotal;
         valorFinanciado = valorTotal - valorEntrada;
@@ -81,27 +83,50 @@ contract CompraVendaVeiculos {
         
     }   
     
-    function pagarEntrada () public payable returns (uint, string memory) {
+    function pagarEntrada (uint _valorEntrada) public payable returns (uint, string memory) {
         require(msg.value == valorEntrada, "valor entrada incorreto");
         require(saldoAberto == valorTotal, "valor da entrada ja paga");
         comprador = msg.sender;
         saldoAberto = valorTotal - msg.value;
         payable(vendedor).transfer(msg.value);
         dataVencimento = block.timestamp + 31 * 86400;
+        emit PagamentoEntrada(comprador, msg.value);
         return (saldoAberto, "Saldo em Aberto");
         
     }
     
     function pagarParcela (uint _valorParcela) public payable returns (uint, string memory) {
         require(_valorParcela == valorParcela, "valor da parcela incorreto");
-        require(saldoAberto <= valorTotal - entrada, "valor da entrada nao foi paga");
+        require(saldoAberto <= (valorTotal - valorEntrada), "valor da entrada nao foi paga");
         require(comprador == msg.sender, "Pagamento nao efetuado. Somente comprador pode realizar o pagamento");
         require(block.timestamp <= dataVencimento, "parcela vencida");
         dataVencimento = dataVencimento + 31 *86400;
         saldoAberto = saldoAberto - _valorParcela;
-        return(saldoAberto, "Saldo em Aberto");
         
+        if (saldoAberto == 0){
+            quitado = true;
+        } else {
+            quitado = false;
+        }
+     
+        emit PagamentoParcela(comprador, msg.value);
+        return(saldoAberto, "Saldo em Aberto");
+     
+    }
+    
+    
+    function pagamentoMulta (uint) public view returns (uint){
+        require(comprador == msg.sender || vendedor == msg.sender, "Apenas o comprador ou vendedor podem executar essa funcao");
+        uint multa;
+        if (block.timestamp > dataVencimento + 31 * 86400 && dataVencimento != 0){
+            multa = porcentagemMulta*valorTotal/100;
+        } else {
+                multa = 0;
+            }
+            return(multa);
         
     }
     
 }
+
+
